d9dcfaf584d53026ed7e9556494f53a619971e70
Tim_Winfred <twinfred@gmail.com>
2018-04-11 19:55:39 -0700

    added django_app, djangoEnv, and main

---
+++ main/apps/first_app/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+
+def index(request):
+    response = "Hello, I am your first request using Django!"
+    return HttpResponse(response)
