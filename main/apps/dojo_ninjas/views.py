aa0cc35fd49ee530329d52b6694b28b705bf0f35
Tim_Winfred <twinfred@gmail.com>
2018-04-16 11:13:31 -0700

    added dojo_ninjas app to 'main' Django project'

---
+++ main/apps/dojo_ninjas/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'dojo_ninjas/index.html')
--- main/main/urls.py
+++ main/main/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^dojo/', include('apps.dojo_ninjas.urls')),
     url(r'^', include('apps.first_app.urls')),
     url(r'^admin/', admin.site.urls)
 ]
