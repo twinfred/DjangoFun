d1a26c85f50f0e4f0eed5b5d9832b446f97605a5
Tim_Winfred <twinfred@gmail.com>
2018-04-17 17:27:02 -0700

    added validation for single email and added if statement to 'index' function in views

--- LoginAndRegistration/apps/login_register/urls.py
+++ LoginAndRegistration/apps/login_register/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^success/$', views.success),
     url(r'^register_account/$', views.register_account),
     url(r'^logout_user/$', views.logout),
     url(r'^login2account/$', views.login),
--- LoginAndRegistration/apps/login_register/views.py
+++ LoginAndRegistration/apps/login_register/views.py
@@ -9,20 +9,19 @@ import bcrypt
 # Create your views here.
 
 def index(request):
-    return render(request, 'login_register/index.html')
-
-def success(request):
     if 'user_id' in request.session:
-        print request.session['user_id']
         context = {
             "user": User.objects.get(id=request.session['user_id'])
         }
         return render(request, 'login_register/success.html', context)
     else:
-        messages.error(request, "You must be logged in.")
-        return redirect('/')
+        return render(request, 'login_register/index.html')
 
 def register_account(request):
+    email_check = User.objects.filter(email = request.POST['email'])
+    if len(email_check):
+        messages.error(request, "Email already exists. Please log in.")
+        return redirect('/')
     if len(request.POST['password']) < 8:
         messages.error(request, "Your password must be at least 8 characters long.")
         return redirect('/')
@@ -43,7 +42,7 @@ def register_account(request):
         new_user = User.objects.create(first_name = first_name, last_name = last_name, email = email, birthday = birthday)
         Password.objects.create(pwd = password, user = User.objects.get(id = new_user.id))
         request.session['user_id'] = new_user.id
-    return redirect ('/success/')
+    return redirect ('/')
 
 def logout(request):
     del request.session['user_id']
@@ -60,7 +59,7 @@ def login(request):
         password = bcrypt.checkpw(request.POST['password'].encode(), user.password.pwd.encode())
         if password == True:
             request.session['user_id'] = user.id
-            return redirect('/success/')
+            return redirect('/')
         else:
             messages.error(request, "Your password is incorrect.")
             return redirect('/')
